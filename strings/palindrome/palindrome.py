0e6cbeeff90d915a8854b0bd024a27b9dd3d2012
Hajeera <hajeerajobs@gmail.com>
2026-01-21 02:04:42 -0800

    Enhance palindrome check with explanatory comments
    Added comments to clarify palindrome check logic.

--- strings/palindrome/palindrome.py
+++ strings/palindrome/palindrome.py
@@ -2,8 +2,8 @@ word = input("Enter word: ")
 
 i = 0
 while i < len(word) // 2:
-    if word[i] != word[len(word) - 1 - i]:
-        print("Not palindrome")
+    if word[i] != word[len(word) - 1 - i]:   #Eg: MADAM word[0]!=word[4]- False ; word[1]!=word[3]0 False  ; word[2]<2 fasle --Out of the while loop
+        print("Not palindrome")              #Eg:HELLO word[0]!=word[4] - True ---Not palindrome
         break
     i = i + 1
 else:
