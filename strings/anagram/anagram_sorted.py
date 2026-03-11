202994d0ca1f0944cf491261ce755efd03d7c540
Hajeera <hajeerajobs@gmail.com>
2026-03-11 16:56:18 -0700

    Add clarification comment for anagram check
    Added comment to clarify the meaning of anagram.

--- strings/anagram/anagram_sorted.py
+++ strings/anagram/anagram_sorted.py
@@ -3,6 +3,6 @@ string2=input("Enter your second string: ").lower()
 
 if sorted(string1)==sorted(string2):  #sorted() function creates a new list containing all items from any iterable 
                                       #(list, tuple, string, set, or dictionary) in ascending order by default, leaving the original iterable unchanged.
-  print("Anagram")
+  print("Anagram") # Anagram means words with same characters in different order
 else:
     print("Not an Anagram")
